01c1f8da140a592eb024028aaa02b460d2cc4e78
Guilherme Miranda <guilhermeinfo1@gmail.com>
2023-08-01 09:42:35 -0300

    Adicionando viewModel

---
+++ RetailApp/RetailApp.ViewModels/Suplier/SupplierViewModel.cs
@@ -0,0 +1,19 @@
+﻿using RetailApp.ViewModels.Address;
+using RetailApp.ViewModels.Phone;
+using System.Collections.Generic;
+
+namespace RetailApp.ViewModels.Suplier
+{
+    public class SupplierViewModel
+    {
+        public int IdSuplier { get; set; }
+        public string CNPJ { get; set; } = default!;
+        public string Name { get; set; } = default!;
+        public string ComercialName { get; set; } = default!;
+        public string IE { get; set; } = default!;
+        public string IEST { get; set; } = default!;
+        public int CRT { get; set; }
+        public List<AddresViewModel> addresViewModel { get; set; } = default!;
+        public List<PhoneViewModel> PhoneViewModel { get; set; } = default!;
+    }
+}
