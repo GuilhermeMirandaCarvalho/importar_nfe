813ef3e970211172bf5ac24ba6282a81f2eda78f
Guilherme Miranda <guilhermeinfo1@gmail.com>
2023-08-01 09:14:57 -0300

    Primeiro Push

---
+++ RetailApp/RetailApp.DesktopUI/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using RetailApp.Utility;
+using RetailApp.ViewModels;
+using RetailApp.ViewModels.Fiscal;
+using System.Windows;
+
+namespace RetailApp.DesktopUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ImportXML();
+        }
+
+        private void ImportXML()
+        {
+            string xmlContent = FileUtility.GetStringFromXml();
+            nfeProc nfeProcObj = XmlUtility.XmlToClass<nfeProc>(xmlContent);
+        }
+       
+    }
+}
