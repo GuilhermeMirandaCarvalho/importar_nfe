c303168aa4c2eaa5337c8130f2b97d4f6dcdc453
Guilherme Miranda <guilhermeinfo1@gmail.com>
2023-08-03 08:41:28 -0300

    Todos os projetos

---
+++ RetailApp/RetailApp.DesktopUI/Register/frmSupplierEdit.xaml.cs
@@ -0,0 +1,26 @@
+﻿using RetailApp.ViewModels.Suplier;
+using System.Windows;
+
+namespace RetailApp.DesktopUI.Register
+{
+    /// <summary>
+    /// Interaction logic for frmSupplierEdit.xaml
+    /// </summary>
+    public partial class frmSupplierEdit : Window
+    {
+        private SupplierViewModel _supplierViewModel;
+
+        public frmSupplierEdit(SupplierViewModel supplierViewModel)
+        {
+            InitializeComponent();
+            _supplierViewModel = supplierViewModel;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.DataContext = _supplierViewModel;
+            dtgAdress.ItemsSource = _supplierViewModel.addresViewModel;
+            dtgPhone.ItemsSource = _supplierViewModel.PhoneViewModel;
+        }
+    }
+}
